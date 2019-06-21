3ddd6495ea5a3a06f74186f537e67c6598ed77b9
asy0239 <asy0239@gmail.com>
2019-06-21 17:45:27 +0900

    scroll add | 190621

--- django_안성윤/matzip/accounts/views.py
+++ django_안성윤/matzip/accounts/views.py
@@ -2,28 +2,34 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 
 def signup(request):
-    if request.method == 'POST':
-        form =UserCreationForm(request.POST)
+    if request.user.is_authenticated:
+        return redirect("posts:index")
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            auth_login(request, user)
             return redirect('posts:index')
     else:
-        form = UserCreationForm()
-    return render(request, 'accounts/signup.html', {'form': form})
+        form = CustomUserCreationForm()
+    return render(request, 'accounts/signup.html', {'form':form})
 
 
 def login(request):
+    if request.user.is_authenticated:
+        return redirect("posts:index")
     if request.method == "POST":
-        form = AuthenticationForm(request, request.POST)  ## request 자체도 넣어 줘야함
+        form = AuthenticationForm(request, request.POST)
         if form.is_valid():
-            auth_login(request, form.get_user())   ## 로그인을 함
+            auth_login(request, form.get_user())
             return redirect('posts:index')
     else:
-        form = AuthenticationForm()   ## 인증하는 작업
-    return render(request, 'accounts/login.html', {'form': form})
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form':form})
 
 
 def logout(request):
